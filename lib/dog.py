4dce0c950a059490e7fba35ccf78ec05f1d14d15
Jacob Sendzik <jake.sendzik@gmail.com>
2024-02-08 13:07:30 -0500

    Complete

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,14 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Fido", breed="Pointer"):
+        if type(name) != str:
+            print("Name must be string between 1 and 25 characters.")
+        elif len(name) < 1 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name
+        if breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self.breed = breed
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,15 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Jake", job="CI Manager"):
+        if type(name) != str:
+            print("Name must be string between 1 and 25 characters.")
+        elif len(name) < 1 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name.title()
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self.job = job
+
